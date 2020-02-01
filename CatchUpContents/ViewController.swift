b2bfc91b2813d23fbfb863a5ea91d51c3893ea15
KiyoshiYamazaki <take_my_time.23@icloud.com>
2020-02-01 11:47:10 +0900

    Initial Commit

---
+++ CatchUpContents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchUpContents
+//
+//  Created by 山崎喜代志 on 2020/02/01.
+//  Copyright © 2020 山崎喜代志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
